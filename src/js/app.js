684ac1afce6b484c364890638bb42d1b4fbb3837
ch3rr17 <cherr.batac@gmail.com>
2016-08-04 20:35:12 -0700

    modified app js

--- src/js/app.js
+++ src/js/app.js
@@ -15,7 +15,7 @@
                 .state('projects.mortgagecalc', { url: '/mortcalc', templateUrl: '/src/templates/mortcalc.html', controller: 'MortgageController as mort' })
                 .state('projects.changecalc', { url: '/changecalc', templateUrl: '/src/templates/changecalc.html', controller: 'ChangeController as change' })
                 .state('projects.weather', { url: '/weather', templateUrl: '/src/templates/weather.html', controller: 'WeatherController as weather' })
-                .state('projects.todo', { url: '/todos', templateUrl: '/src/templates/todo.html', controller: 'ToDoController as todo' });
+                .state('projects.todo', { url: '/todos', templateUrl: '/src/templates/todo.html', controller: 'ToDoController as todo' })
 
         })
 
