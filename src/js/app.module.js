2f5f98dff35a554dd8f8359511eca596342d43e2
ch3rr17 <cherr.batac@gmail.com>
2016-08-04 18:49:59 -0700

    gulp and file changes

--- src/js/app.module.js
+++ src/js/app.module.js
@@ -7,15 +7,15 @@
             'toastr'
         ])
         .config(['$stateProvider', '$urlRouterProvider', '$httpProvider',  function($stateProvider, $urlRouterProvider, $httpProvider){
-        	$urlRouterProvider.otherwise('main');
+        	$urlRouterProvider.otherwise('/main');
             
             //Create state for each page
-        	$stateProvider.state('main', { url: '/main', templateUrl: '/templates/main.html', controller: 'MainController as main'})
-        				  .state('projects', { url:'/projects', templateUrl: '/templates/projects.html', controller: 'ProjectController as project'})
-                          .state('projects.mortgagecalc', { url:'/mortcalc', templateUrl:'/templates/mortcalc.html', controller:'MortgageController as mort'})
-                          .state('projects.changecalc', {url:'/changecalc', templateUrl:'/templates/changecalc.html', controller:'ChangeController as change'})
-                          .state('projects.weather', {url:'/weather', templateUrl:'/templates/weather.html', controller:'WeatherController as weather'})
-                          .state('projects.todo', {url:'/todos', templateUrl:'/templates/todo.html', controller:'ToDoController as todo'});
+        	$stateProvider.state('main', { url: '/main', templateUrl: '/src/templates/main.html', controller: 'MainController as main'})
+        				  .state('projects', { url:'/projects', templateUrl: '/src/templates/projects.html', controller: 'ProjectController as project'})
+                          .state('projects.mortgagecalc', { url:'mortcalc', templateUrl:'/src/templates/mortcalc.html', controller:'MortgageController as mort'})
+                          .state('projects.changecalc', {url:'/changecalc', templateUrl:'/src/templates/changecalc.html', controller:'ChangeController as change'})
+                          .state('projects.weather', {url:'/weather', templateUrl:'/src/templates/weather.html', controller:'WeatherController as weather'})
+                          .state('projects.todo', {url:'/todos', templateUrl:'/src/templates/todo.html', controller:'ToDoController as todo'});
         				  
         }]);
         
